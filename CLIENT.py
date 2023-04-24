16fd7b32def9fdb148096ae1149c6af6887050e2
HamzaK702 <125295517+HamzaK702@users.noreply.github.com>
2023-04-24 19:42:13 +0500

    Add files via upload

---
+++ CLIENT.py
@@ -0,0 +1,124 @@
+import socket
+import pyaudio
+import tkinter as tk
+import threading 
+import os
+from tkinter import filedialog
+
+# Constants
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = 1024
+HOST = '127.0.0.1' # Change this to the IP address or hostname of the server
+PORT = 12345
+
+# Create a socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+
+def choose_file():
+    file_path = filedialog.askopenfilename()
+    entry_file_path.delete(0, tk.END)
+    entry_file_path.insert(tk.END, file_path)
+
+
+def send_file():
+    file_path = entry_file_path.get()
+    if not file_path:
+        tk.messagebox.showerror("Error", "Please choose a file to send.")
+        return
+
+    try:
+        # # Define the server address and port
+        # SERVER_ADDRESS = '127.0.0.1'  # localhost
+        # SERVER_PORT = 12345
+
+        # # Create a socket object
+        # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # # Connect to the server
+        # client_socket.connect((SERVER_ADDRESS, SERVER_PORT))
+
+        # Get the file name from the file path
+        file_name = os.path.basename(file_path)
+
+        # Send the file name and file size to the server
+        file_size = os.path.getsize(file_path)
+        client_socket.send(f"{file_name}|{file_size}".encode())
+
+        # Open the file and send file data to the server in chunks
+        with open(file_path, 'rb') as file:
+            while file_size > 0:
+                data = file.read(1024)
+                client_socket.send(data)
+                file_size -= len(data)
+
+        print("File sent successfully.")
+
+        # Close the socket
+        client_socket.close()
+
+    except Exception as e:
+        tk.messagebox.showerror("Error", f"Failed to send file: {e}")
+
+
+
+# Create an instance of PyAudio
+p = pyaudio.PyAudio()
+
+# Open a streaming stream as output
+stream_out = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                output=True,
+                frames_per_buffer=CHUNK)
+
+# Function to stop the client
+def stop_client():
+    client_socket.close()
+    p.terminate()
+    root.quit()
+
+# Function to handle audio playback from the server
+def play_audio():
+    while True:
+        data = client_socket.recv(CHUNK)
+        stream_out.write(data)
+
+# Create a GUI window
+root = tk.Tk()
+root.title("Audio Client")
+
+# Add a label
+label = tk.Label(root, text="CLIENT", font=("Helvetica", 16))
+label.pack(pady=20)
+
+# Create file path entry
+entry_file_path = tk.Entry(root, width=50)
+entry_file_path.pack(pady=10)
+
+# Create buttons for choosing file and sending file
+btn_choose_file = tk.Button(root, text="Choose File", command=choose_file)
+btn_choose_file.pack()
+btn_send_file = tk.Button(root, text="Send File", command=send_file)
+btn_send_file.pack(pady=10)
+
+# Create buttons for choosing audio and playing audio
+# btn_browse_audio = tk.Button(root, text="Browse Audio", command=browse_audio)
+# btn_browse_audio.pack()
+# btn_play_audio = tk.Button(root, text="Play Audio", command=play_audio)
+# btn_play_audio.pack(pady=10)
+
+
+
+# Add a button to stop the client
+stop_button = tk.Button(root, text="Stop Client", font=("Helvetica", 14), command=stop_client)
+stop_button.pack()
+
+# Start audio playback thread
+audio_thread = threading.Thread(target=play_audio)
+audio_thread.start()
+
+# Start the GUI event loop
+root.mainloop()
---
+++ SERVER.py
@@ -0,0 +1,120 @@
+import socket
+import threading
+import tkinter as tk
+from tkinter import filedialog
+import pyaudio
+import wave
+import os
+
+# Constants
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = 1024
+HOST = '127.0.0.1' # Change this to the IP address or hostname of the server
+PORT = 12345
+
+# Create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen(5)
+print(f"Server is listening on {HOST}:{PORT}")
+
+# List to store connected client sockets
+client_sockets = []
+
+# Create an instance of PyAudio
+p = pyaudio.PyAudio()
+
+# Function to play audio from a file
+def play_audio(filename):
+    wf = wave.open(filename, 'rb')
+    stream_out = p.open(format=FORMAT,
+                        channels=CHANNELS,
+                        rate=RATE,
+                        output=True,
+                        frames_per_buffer=CHUNK)
+    data = wf.readframes(CHUNK)
+    while data:
+        for client_socket in client_sockets:
+            client_socket.sendall(data)
+        data = wf.readframes(CHUNK)
+    stream_out.stop_stream()
+    stream_out.close()
+
+# Function to stop the server
+def stop_server():
+    for client_socket in client_sockets:
+        client_socket.close()
+    server_socket.close()
+    p.terminate()
+    root.quit()
+
+# Function to handle client connections
+def handle_clients():
+    while True:
+        client_socket, client_addr = server_socket.accept()
+        client_sockets.append(client_socket)
+        print(f"Connection from {client_addr} established.")
+        
+
+# def broadcast():
+#     client_thread = threading.Thread(target=play_audio(), args="filename.wav")
+#     client_thread.start()
+    
+         # Receive the file name and file size from the client
+         # if not file_name.endswith(".mp3"):
+    #     file_info = client_socket.recv(1024).decode()
+    #     file_name, file_size = file_info.split('|')
+
+    # # Convert file size to integer
+    #     file_size = int(file_size)
+
+    # # Create a new file to write the received data
+    #     with open(file_name, 'wb') as file:
+    #     # Loop to receive file data in chunks
+    #      while file_size > 0:
+    #         data = client_socket.recv(1024)
+    #         file.write(data)
+    #         file_size -= len(data)
+
+    #      print(f"File '{file_name}' received and saved successfully.")
+ 
+
+# Create a GUI window
+root = tk.Tk()
+root.title("Audio Server")
+
+# Add a label
+label = tk.Label(root, text="Select an audio file to play:", font=("Helvetica", 16))
+label.pack(pady=20)
+
+# Function to open file dialog and select a file
+def select_file():
+    filename = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select File", filetypes=(("WAV files", "*.wav"), ("All files", "*.*")))
+    print(f"{filename} selected for broadcasting")
+    if filename:
+        play_audio(filename)
+            
+
+
+
+# Add a button to select a file
+select_button = tk.Button(root, text="Select Audio to broadcast", font=("Helvetica", 14), command=select_file)
+select_button.pack()
+
+
+#broadcast_button = tk.Button(root, text="Start broadcasting audio", font=("Helvetica", 14), command=broadcast)
+#broadcast_button.pack()
+# Add a button to stop the server
+stop_button = tk.Button(root, text="Stop Server", font=("Helvetica", 14), command=stop_server)
+stop_button.pack()
+
+
+
+# Start client handling thread
+client_thread = threading.Thread(target=handle_clients)
+client_thread.start()
+
+# Start the GUI event loop
+root.mainloop()
